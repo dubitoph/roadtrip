eb107dac724347c908f4efbb9c2011a6b8bea4aa
dubitoph <47710530+dubitoph@users.noreply.github.com>
2019-10-22 14:14:55 +0200

    Custom select bug fixed

--- assets/js/actions/select/check_active.js
+++ assets/js/actions/select/check_active.js
@@ -3,7 +3,17 @@ export function checkActive(e, options, head, select,customSelect,count,openH, s
 
 	if(e.target.localName != "li") return;
 	options.forEach( o => e.target != o ? o.classList.remove('isActive') : o.classList.add('isActive') )
+
+/*
 	selHide.children.forEach( o => e.target.getAttribute('rel') === o.value ? o.setAttribute('selected', 'selected') : o.removeAttribute('selected') )
+*/
+	
+	Array.prototype.forEach.call(selHide.children, child => {
+		o => e.target.getAttribute('rel') === o.value ? o.setAttribute('selected', 'selected') : o.removeAttribute('selected')
+	});
+
 	head.innerHTML = e.target.innerHTML
 	showHide(select,customSelect,count,openH)
+	
+
 }
