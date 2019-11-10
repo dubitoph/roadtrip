3201f410510476ec60857bfafe9224a2af33a53e
dubitoph <47710530+dubitoph@users.noreply.github.com>
2019-11-10 15:50:02 +0100

    Fuel management flow updated

--- src/Form/backend/FuelType.php
+++ src/Form/backend/FuelType.php
@@ -14,8 +14,7 @@ class FuelType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
 
-        $builder
-            ->add('fuel', TextType::class)
+        $builder->add('fuel', TextType::class)
         ;
 
     }
